23c640eb19847148690351b1dca137f691a0c07e
Sharan Kalyan <sharandhoni2526@gmail.com>
2022-02-23 11:11:04 +0530

    Add files via upload

--- historical_training.py
+++ historical_training.py
@@ -18,6 +18,7 @@ from sklearn.metrics import r2_score,mean_squared_error, confusion_matrix
 from catboost import CatBoostRegressor
 # from lightgbm import LGBMRegressor
 import datetime
+import pickle
 
 
 # In[9]:
@@ -34,70 +35,14 @@ def main():
 	# db_cursor.execute('SELECT * FROM aq_data5')
 	# table_rows = db_cursor.fetchall()
 	# b = pd.DataFrame(table_rows,columns=['station','date','day','year','month','time','pm25','pm10','nox','temperature','co'])
-
-
-	# In[41]:
-
-
 	# b.to_csv('aqdb_data.csv',index=False)
-
-
-	# In[42]:
-
-
 	# a = pd.read_csv('aqdb_data.csv')
-
-
-	# In[11]:
-
-
-	a = pd.read_csv('2020-2022-feb13.csv') ## REading data from .csv file
-
-
-	# In[12]:
-
-
+	a = pd.read_csv('2020-2022-feb21.csv') ## REading data from .csv file
 	l2 = a.copy()
-
-
-	# In[13]:
-
-
-	l2.head()
-
-
-	# In[14]:
-
-
-	l2.isnull().sum()
-
-
-	# In[15]:
-
-
+	## Preprocessing the data
 	l2['month'] = l2.month.astype(int)
 	l2['year'] = l2.year.astype(int)
-
-
-	# In[16]:
-
-
-	l2.head()
-
-
-	# ### Preprocessing the Data
-
-	# In[17]:
-
-
 	# l2['Date'] = pd.to_datetime(l2['Date'])
-
-	# years = []
-	# yrs = l2.Date.dt.year
-	# for i in yrs:
-	#     years.append(i)
-	# l2['Year'] = years
-
 	hr =[]
 	for i in l2.time:
 		if len(i)>5:
@@ -105,7 +50,6 @@ def main():
 		elif len(i) <=5:
 		    hr.append(i[:-3])
 	l2['hour'] = hr
-
 	## if you want to have 30 min data
 	# h = []
 	# for i in l2.hour:
@@ -113,34 +57,9 @@ def main():
 	#         h.append(i[1:-3]+'.0')
 	#     elif i.endswith('30'):
 	#         h.append(i[1:-3]+'.5')
-	# l2['hour'] = h 
-
+	# l2['hour'] = h
 	l2['hour']= l2.hour.astype(int)
-
-
-	# In[18]:
-
-
 	l2 = l2[['station','date','day','time','year','month','hour','pm25','pm10','nox','temperature','co']]
-
-
-	# In[19]:
-
-
-	l2.head()
-
-
-	# ### Processing Stations to Integers
-
-	# In[20]:
-
-
-	# l2.head()
-
-
-	# In[21]:
-
-
 	station_processed = []
 	for i in l2.station:
 		if i == 'Kodungaiyur_Chennai_TNPCB':
@@ -154,8 +73,6 @@ def main():
 		elif i == 'Velacheri_Res_Area_CPCB':
 		    station_processed.append(5)
 	l2['station'] = station_processed
-
-
 	d = []
 	for i in l2.day:
 		if i == 'Sunday':
@@ -173,18 +90,13 @@ def main():
 		elif i == 'Saturday':
 		    d.append(7)
 	l2['day'] = d
-		    
-
-
-	## months 
+	## months
 	## 1,2 - Winter (1)
 	## 3,4 - spring (2)
 	## 5,6 - summer (3)
 	## 7,8 - monsoon (4)
 	## 9,10 - autumn (5)
 	## 11,12 - pre-winter (6)
-
-
 	season = []
 	for i in l2.month:
 		if i <= 2:
@@ -200,9 +112,6 @@ def main():
 		elif i == 11 or i == 12:
 		    season.append(6)
 	l2['season'] = season
-
-
-
 	## 12 - 4 Midnight:
 	## 4 - 7 - Early-Morning
 	## 7 -12 - morning
@@ -223,41 +132,11 @@ def main():
 		    pod.append(5)
 		elif i>=19 and i <= 23:
 		    pod.append(6)
-
 	l2['part_of_day'] = pod
-
-
-	# In[ ]:
-
-
-
-
-
-	# ### Dropping Date & Time feature (because of hour feature being added as an integer) 
-
-	# In[22]:
-
-
+	## Dropping Date & Time feature (because of hour feature being added as an integer)
 	l2.drop(['date','time'],axis=1,inplace=True)
-
-
-	# In[23]:
-
-
 	l2 = l2[['station','day','month','season','year','hour','part_of_day','pm10','nox','temperature','co','pm25']]
-
-
-	# In[ ]:
-
-
-
-
-
 	# ### Preparing Test Data - 5 days of test data from current date.
-
-	# In[25]:
-
-
 	import datetime
 	data = []
 	stations = [1, 2, 3, 4, 5]
@@ -268,14 +147,7 @@ def main():
 		    for time in range(0,24):
 		        data.append({'station': s, 'date': date, 'hour': time})
 	test_data = pd.DataFrame(data)
-
-
 	# ### Custom predictions - If the predictions needs to be ran for a custom date
-
-	# In[27]:
-
-
-
 	# import datetime
 	# data = []
 	# stations = [1, 2, 3, 4, 5]
@@ -287,68 +159,37 @@ def main():
 	#         for time in range(0,24):
 	#             data.append({'station': s, 'date': date, 'hour': time})
 	# test_data = pd.DataFrame(data)
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[30]:
-
-
-	# test_data.head()
-
-
-	# In[31]:
-
-
-	#test_data = pd.read_csv('test-data-allParams-25th-5thSep-hourly.csv')
 	test_data['date'] = pd.to_datetime(test_data['date'])
-
-
 	months1 = []
 	years1 = []
 	day1 = []
 	mth1 = test_data.date.dt.month
 	yrs1 = test_data.date.dt.year
 	dy1 = test_data.date.dt.day_name()
-
 	for i in mth1:
 		months1.append(i)
 	test_data['month'] = months1
-
 	for i in yrs1:
 		years1.append(i)
 	test_data['year'] = years1
-
 	for i in dy1:
 		test_data['day'] = dy1
-
-
 	# hour1 =[]
 	# for i in test_data.Time:
 	#     if len(i) > 5:
 	#         hour1.append(i[:-6])
 	#     elif len(i) <=5:
 	#         hour1.append(i[:-3])
-
-	# test_data['Hour'] = hour1 
-
+	# test_data['Hour'] = hour1
 	# test_data['Hour']= test_data.Hour.astype(int)
 
-
-
-	## months 
+	## months
 	## 1,2 - Winter (1)
 	## 3,4 - spring (2)
 	## 5,6 - summer (3)
 	## 7,8 - monsoon (4)
 	## 9,10 - autumn (5)
 	## 11,12 - pre-winter (6)
-
-
 	season = []
 	for i in test_data.month:
 		if i <= 2:
@@ -364,7 +205,6 @@ def main():
 		elif i == 11 or i == 12:
 		    season.append(6)
 	test_data['season'] = season
-
 	pod = []
 	for i in test_data.hour:
 		if i >= 0 and i < 4:
@@ -379,19 +219,7 @@ def main():
 		    pod.append(5)
 		elif i>=19 and i <= 23.5:
 		    pod.append(6)
-
 	test_data['part_of_day'] = pod
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[32]:
-
-
 	d = []
 	for i in test_data.day:
 		if i == 'Sunday':
@@ -409,185 +237,76 @@ def main():
 		elif i == 'Saturday':
 		    d.append('7')
 	test_data['day'] = d
-		    
 
-
-	# In[ ]:
-
-
-
-
-
-	# In[33]:
-
-
-	test_data.head()
-
-
-	# In[35]:
-
-
-	l2.head()
-
-
-	# In[ ]:
-
-
-
-
-
-	# # Models
-
-	# In[37]:
-
-
+	## Models
 	cat = CatBoostRegressor(verbose=False)
 	#lgb = LGBMRegressor()
 
-
-	# # Predicting NOx
-
-	# In[38]:
-
-
-	import pickle
-
-
-	# In[39]:
-
-
+	## Predicting NOx
 	nox_pred = l2.copy()
 	nox_pred.drop(['pm10','temperature','co','pm25'],axis=1,inplace=True)
 	nox_pred.dropna(inplace=True)
-
 	x = nox_pred.drop('nox',1)
 	y = nox_pred['nox']
-
 	nox_test = test_data.copy()
 	nox_test = nox_test[['station','date','day','month','season','year','hour','part_of_day']]
 	nox_test.dropna(inplace=True)
 	x_test = nox_test
-
 	cat = cat.fit(x,y)
 	cat_test_pred = cat.predict(x_test)
 	pkl_file = './weight_files/nox.pkl'
 	pickle.dump(cat, open(pkl_file, 'wb'))
-
 	cat_test_pred = list(cat_test_pred)
 	final_results = nox_test.copy()
 	final_results['predicted_nox'] = cat_test_pred
-	final_results.head()
+	#final_results.head()
 
-
-	# In[ ]:
-
-
-
-
-
-	# # Predicting Temperature
-
-	# In[30]:
-
-
-	# temp_pred.shape
-
-
-	# In[40]:
-
-
+	## Predicting Temperature
 	temp_pred = l2.copy()
 	temp_pred.drop(['pm10','nox','co','pm25'],axis=1,inplace=True)
-
 	temp_pred.dropna(inplace=True)
-
 	x = temp_pred.drop('temperature',1)
 	y = temp_pred['temperature']
-
 	## Testing it with Test data
-
 	temp_test = test_data.copy()
 	temp_test = temp_test[['station','day','month','season','year','hour','part_of_day']]
 	temp_test.dropna(inplace=True)
 	x_test = temp_test
-
 	cat = cat.fit(x,y)
 	cat_test_pred = cat.predict(x_test)
-
 	pkl_file = './weight_files/temp.pkl'
 	pickle.dump(cat, open(pkl_file, 'wb'))
 	cat_test_pred = list(cat_test_pred)
 	final_results['predicted_temperature'] = cat_test_pred
-	final_results.head()
+	#final_results.head()
 
-
-	# In[ ]:
-
-
-
-
-
-	# # Predicting CO
-
-	# In[32]:
-
-
-	# co_pred.shape
-
-
-	# In[41]:
-
-
+	## Predicting CO
 	co_pred = l2.copy()
-
 	co_pred.drop(['pm10','nox','temperature','pm25'],axis=1,inplace=True)
-
 	co_pred.dropna(inplace=True)
 	co_pred.head()
-
-
 	x = co_pred.drop('co',1)
 	y = co_pred['co']
-
 	### Testing it on test data
-
 	co_test = test_data.copy()
 	co_test = co_test[['station','day','month','season','year','hour','part_of_day']]
 	co_test.dropna(inplace=True)
 	x_test = co_test
-
 	cat = cat.fit(x,y)
 	cat_test_pred = cat.predict(x_test)
 	cat_test_pred = list(cat_test_pred)
-
 	pkl_file = './weight_files/co.pkl'
 	pickle.dump(cat, open(pkl_file, 'wb'))
-
 	final_results['predicted_co'] = cat_test_pred
-
 	#final_results.head()
 
-
-	# In[ ]:
-
-
-
-
-
-	# # Predicting PM2.5
-
-	# In[42]:
-
-
+	## Predicting PM2.5
 	pm25_pred = l2.copy()
-
 	pm25_pred.drop(['pm10','nox','temperature','co'],axis=1,inplace=True)
 	pm25_pred.dropna(inplace=True)
 	x = pm25_pred.drop('pm25',1)
 	y = pm25_pred['pm25']
-
 	### Testing it on test data
-
 	pm25_test = test_data.copy()
 	pm25_test = pm25_test[['station','day','month','season','year','hour','part_of_day']]
 	pm25_test.dropna(inplace=True)
@@ -600,52 +319,23 @@ def main():
 	final_results['predicted_pm25'] = cat_test_pred
 	#final_results.head()
 
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[43]:
-
-
-	final_results.head()
-
-
-	# ### Processing the predictions to user readable format
-
-	# In[44]:
-
-
-	## Date formating
+	## Processing the predictions to user readable format
 	final_results['date'] = final_results['date'].astype(str)
-
-
-	## Station int - str 
+	## Station int - str
 	sp = []
 	for i in final_results.station:
 		if i == 1:
 		    sp.append('Kodungaiyur_Chennai_TNPCB')
 		elif i == 2:
 		    sp.append('Royapuram_Chennai_TNPCB')
-		elif i == 3: 
+		elif i == 3:
 		    sp.append('Perungudi_Chennai_TNPCB')
-		elif i == 4: 
+		elif i == 4:
 		    sp.append('Alandur_Bus_Depot_Chennai_CPCB')
 		elif i == 5:
 		    sp.append('Velacheri_Res_Area_CPCB')
 	final_results['station'] = sp
-
-
 	## AQI - int - str
-
 	# 1 - good
 	# 2 - Satisfactory
 	# 3 - Moderate
@@ -666,10 +356,7 @@ def main():
 		    iqa.append('very poor')
 		elif i >= 251:
 		    iqa.append('severe')
-
 	final_results['predicted_aqi'] = iqa
-
-
 	# 1 - good
 	# 2 - Satisfactory
 	# 3 - Moderate
@@ -677,35 +364,13 @@ def main():
 	# 5 - very Poor
 	# 6 - Severe
 
-	# iqa = []
-	# for i in final_results['Actual_PM2.5']:
-	#     if i < 31:
-	#         iqa.append('good')
-	#     elif i >=31 and i < 61:
-	#         iqa.append('satisfactory')
-	#     elif i >= 61 and i < 91:
-	#         iqa.append('moderate')
-	#     elif i >= 91 and i < 121:
-	#         iqa.append('poor')
-	#     elif i >= 121 and i < 251:
-	#         iqa.append('very poor')
-	#     elif i >= 251:
-	#         iqa.append('severe')
-	#     else:
-	#         iqa.append('Unknown')
-
-	# final_results['Actual_AQI'] = iqa
-
-
 	## part of day int - str
-
 	## 12 - 4 Midnight:
 	## 4 - 7 - Early-Morning
 	## 7 -12 - morning
 	## 12 - 16 - Afternoon
 	## 16 - 19 - Evening
 	## 19 - 23 - Night
-
 	dop = []
 	for i in final_results.part_of_day:
 		if i == 1:
@@ -722,18 +387,13 @@ def main():
 		    dop.append('Night')
 	final_results.part_of_day = dop
 
-
-	# In[45]:
-
-
-	## months 
+	## months
 	## 1,2 - Winter (1)
 	## 3,4 - spring (2)
 	## 5,6 - summer (3)
 	## 7,8 - monsoon (4)
 	## 9,10 - autumn (5)
 	## 11,12 - pre-winter (6)
-
 	season = []
 	for i in final_results.month:
 		if i <= 2:
@@ -748,10 +408,7 @@ def main():
 		    season.append('Autumn')
 		elif i == 11 or i == 12:
 		    season.append('Pre-winter')
-		    
 	final_results['season'] = season
-
-
 	hor = []
 	for i in final_results.hour:
 		if i == 0:
@@ -761,7 +418,7 @@ def main():
 		elif i == 2:
 		    hor.append('02:00')
 		elif i == 3:
-		    hor.append('03:00') 
+		    hor.append('03:00')
 		elif i == 4:
 		    hor.append('04:00')
 		elif i == 5:
@@ -802,13 +459,7 @@ def main():
 		    hor.append('22:00')
 		elif i == 23:
 		    hor.append('23:00')
-		    
 	final_results['hour'] = hor
-
-
-	# In[46]:
-
-
 	mont = []
 	for i in final_results.month:
 		if i == 1:
@@ -816,7 +467,7 @@ def main():
 		elif i == 2:
 		    mont.append('February')
 		elif i == 3:
-		    mont.append('March') 
+		    mont.append('March')
 		elif i == 4:
 		    mont.append('April')
 		elif i == 5:
@@ -835,133 +486,21 @@ def main():
 		    mont.append('November')
 		elif i == 12:
 		    mont.append('December')
-		    
 	final_results['month'] = mont
-	
 	print('Weights succesfully saved to ./weight_files/')
-
-
-	# In[41]:
-
-
-	# final_results.to_csv('ftmodel_nov18.csv',index=False)
-
-
-	# In[48]:
-
-
 	# import time
 	# timestr = time.strftime("%Y-%m-%d - %H")
 	# final_results.to_csv('./Predictions/predictions_base_-'+str(timestr)+'Hours'+'.csv', index = False)
 
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[49]:
-
-
-	# final_grouped_date = final_results[['predicted_pm25']].groupby(final_results.date).mean()
-
-
-	# In[50]:
-
-
-	# print('\nDate Wise Average predictions for PM2.5')
-	# print('\n')
-	# print(final_grouped_date)
-
-
-	# In[ ]:
-
-
-
-
-
-	# ### Sending the predictions to the predictions table in DB
-
-	# In[51]:
-
-
+	## Sending the predictions to the predictions table in DB
 	# final_results.drop('day',axis=1,inplace=True)
-
-
-	# In[46]:
-
-
 	# import sqlalchemy
 	# #final_results =  # The table we need to upload, here - final_grouped_date
-
-
 	# database_username = 'root'
 	# database_password = 'sharan123'
 	# database_ip       = '127.0.0.1'
 	# database_name     = 'aq_historical_data'
 	# database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
-	#                                                format(database_username, database_password, 
+	#                                                format(database_username, database_password,
 	#                                                       database_ip, database_name))
 	# final_results.to_sql(con=database_connection, name='aq_predictions1', if_exists='append',index=False)
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
-
-	# In[ ]:
-
-
-
-
